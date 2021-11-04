a5571c09b9c943b6307e258f4fc6b3e7cdef24f9
junyao08 <junyao5071@gmail.com>
2021-11-04 21:38:51 +0800

    commit

--- go-projects/web-service-gin/main.go
+++ go-projects/web-service-gin/main.go
@@ -1,74 +1,15 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusOK, newAlbum)
-}
-
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	for _, album := range albums {
-		if album.ID == id {
-			c.IndentedJSON(http.StatusOK, album)
-			return
-		}
-	}
-
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album id not found"})
-}
-
-func deleteAlbumById(c *gin.Context) {
-	id := c.Param("id")
-
-	intId, _ := strconv.Atoi(id)
-
-	remainingAlbums := remove(albums, intId)
-	c.IndentedJSON(http.StatusOK, remainingAlbums)
-}
-
-func remove(slice []album, index int) []album {
-	return append(slice[:index], slice[index-1:]...)
-}
-
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.GET("/albums/delete/:id", deleteAlbumById)
+	router.GET("/albums", controllers.getAlbums)
+	router.POST("/albums", controllers.postAlbums)
+	router.GET("/albums/:id", controllers.getAlbumByID)
+	router.GET("/albums/delete/:id", controllers.deleteAlbumById)
 
 	router.Run("localhost:8090")
 }
